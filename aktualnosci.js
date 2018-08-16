bd2d4407efc1f1c4f0351570c4fb9c3809b427b2
Moczu17 <moczarscy@vp.pl>
2018-08-16 21:19:49 +0200

    aktualnosci ver. 2.0

---
+++ aktualnosci.js
@@ -0,0 +1,14 @@
+﻿function aktualnosci(){
+	var aktualnosci = 
+		'<div id="tlo">'+
+			'<span class="title1">23.08.2018</span><br />'+
+			'To jest przykładowy tekst, żebyście wiedzieli jak dodawać treści.  '+
+			'Korzystajcie z tego wzoru. '+
+			'Jest on wykomentowany, żeby nie pojawiał się na stronie. '+
+			'Owocnej nauki JS i HTML! :D '+
+		'</div>'+
+		
+		'<button class="but2" onclick="wroc();">WRÓĆ</button>';
+		
+		document.getElementById('main').innerHTML = aktualnosci;
+}
